43943958043eedcea0d36c91c672db5b4b09edbd
Pehuén <pehuen.rodriguez@nayra.coop>
2020-02-02 19:09:03 -0300

    Index and edit views

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,8 +3,13 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
-    changeset = Topic.changeset(%Topic{}, %{})
+    changeset = Topic.changeset(%Topic{})
     render conn, "new.html", changeset: changeset
   end
 
@@ -12,9 +17,32 @@ defmodule Discuss.TopicController do
      changeset = Topic.changeset(%Topic{}, topic)
 
      case Repo.insert(changeset) do
-       {:ok, post} -> IO.inspect(post)
+       {:ok, _topic} ->
+          conn
+          |> put_flash(:info, "Topic created")
+          |> redirect(to: topic_path(conn, :index))
        {:error, changeset} ->
           render conn, "new.html", changeset: changeset
      end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{ "id" => topic_id, "topic" => topic }) do
+    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset
+    end
   end
 end
